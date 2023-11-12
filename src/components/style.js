0b3dbc2f5d6e2e1dc6617ff7cbae30f2033f37df
AshArce <arcebuchejohnashley@gmail.com>
2023-11-12 18:10:44 +0800

    update

--- src/components/style.js
+++ src/components/style.js
@@ -1,4 +1,3 @@
-
 import { Grid } from "@mui/material";
 import { styled } from "@mui/system";
 
@@ -97,139 +96,23 @@ const styles = {
     height: "250px",
     borderRadius: 50,
   },
-};
-export default styles;
-
-//Footer
-export const Root = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "flex-start",
-  justifyContent: "start",
-  margin: "0 11px",
-  width: "95%",
-  [theme.breakpoints.up("md")]: {
-    flexDirection: "column",
-    gap: "5px",
-    marginLeft: "0",
-    width: "100%",
-  },
-}));
-
-export const Column = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  gap: "2.5px",
-  height: "auto",
-  alignItems: "flex-start",
-  justifyContent: "start",
-  width: "auto",
-  [theme.breakpoints.up("md")]: {
-    marginLeft: "0",
-    [theme.breakpoints.up("md")]: {
-      marginLeft: "123px",
-    },
-    [theme.breakpoints.up("lg")]: {
-      marginLeft: "99px",
-    },
-  },
-}));
 
   //Dashboard
-  
-     dashboardContainer: {
-      display: 'flex',
-    },
-    sidebar: {
-      width: '250px', // Adjust the width of the sidebar as needed
-      backgroundColor: '#333', // Set the background color for the sidebar
-      color: 'white', // Set text color for the sidebar
-    },
-    dashboardContent: {
-      flex: 1,
-      padding: '20px', // Adjust padding as needed
-      backgroundColor: '#f0f0f0', // Set background color for content
-      overflowY: 'auto', // Add vertical scroll if content overflows
-    },
-  //App Bar
-  appBar: {
-    backgroundColor: "transparent",
-    color: "green",
-  },
 
-  greenBoxBg: {
-    height: "auto",
-    backgroundColor: "#39A848",
-  },
-
-  container: {
-    position: "relative",
-    // backgroundColor: 'theme.palette.common.white',
+  dashboardContainer: {
     display: "flex",
-    border: "1px solid #000",
-    height: 250,
-    width: 300,
-    borderRadius: 30,
-    boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.1)",
   },
-  imageContainer: {
-    position: "absolute",
-    backgroundColor: "white",
-    bottom: 0,
-    insetX: 0,
-    marginLeft: "auto",
-    marginRight: "auto",
-    objectFit: "cover",
-    borderRadius: 52,
-    width: "100%",
+  sidebar: {
+    width: "250px", // Adjust the width of the sidebar as needed
+    backgroundColor: "#333", // Set the background color for the sidebar
+    color: "white", // Set text color for the sidebar
   },
-  textContainer: {
-    display: "flex",
-    flexDirection: "column",
-    gap: 39,
-    height: 108,
-    alignItems: "start",
-    maxWidth: 773,
-    marginLeft: -263,
-    marginTop: 23,
-    zIndex: 1,
-  },
-  description: {
-    fontWeight: "bold",
-    fontFamily: "Poppins",
-    maxWidth: 773,
-    fontSize: "12px",
-    // color: theme.palette.light_green[900],
-  },
-  button: {
-    // color: theme.palette.common.white,
-    fontWeight: "bold",
-    fontFamily: "Poppins",
-    display: "flex",
-    justifyContent: "center",
-    textTransform: "none",
-    paddingLeft: 11,
-    paddingRight: 2,
-    borderRadius: 7,
-    fontSize: "10px",
-    textAlign: "center",
-    width: 101,
-  },
-  //Images
-  ladyShopping: {
-    width: "130px",
-    height: "130px",
-  },
-  RegisterBoxVector: {
-    display: "flex",
-    bottom: "-35px",
-    alignItems: "end",
-    position: "absolute",
-    width: "300x",
-    height: "250px",
-    borderRadius: 50,
+  dashboardContent: {
+    flex: 1,
+    padding: "20px", // Adjust padding as needed
+    backgroundColor: "#f0f0f0", // Set background color for content
+    overflowY: "auto", // Add vertical scroll if content overflows
   },
 };
 
 export default styles;
-
